2a699e77295ecd2dbae75346bd4ef305ab795b8f
Ryota Tomiyama <lit.karu.noir@gmail.com>
2018-09-06 04:52:22 +0000

    Lmplement basic login

--- config/puma.rb
+++ config/puma.rb
@@ -15,6 +15,28 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+# https://railstutorial.jp/chapters/sign_up?version=5.1#fig-signup_failure_rails_debug
+# ここから
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
+# ここまで
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
